812c5c25857dcaf95faefd5172b5e9b9935b390b
Nigrimmist <Nigrimmist@gmail.com>
2020-01-24 21:51:39 +0300

    proj link fixes

--- AwsLambdaHandleTelegramWebhooks/Function.cs
+++ AwsLambdaHandleTelegramWebhooks/Function.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Amazon.Lambda.Core;
-using AwsLamdaRssCore;
 using System.Net;
 using Amazon.Lambda.APIGatewayEvents;
 using AwsLamdaCore.Managers;
